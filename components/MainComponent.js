ca26cee09fccea6104f33af66784d4195535daab
Hayat Ibrahim <hayusnn@gmail.com>
2020-05-05 16:11:55 -0500

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -8,6 +8,7 @@ DrawerItems } from 'react-navigation';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoritesComponent';
 import { Icon } from 'react-native-elements';
 import SafeAreaView from 'react-native-safe-area-view';
 import { connect } from 'react-redux';
@@ -145,6 +146,29 @@ const ReservationNavigator = createStackNavigator(
     }
 );
 
+const FavoritesNavigator = createStackNavigator(
+    {
+        Favorites: { screen: Favorites}
+    },
+    {
+        navigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+            name='heart'
+            type='font-awesome'
+            iconStyle={StyleSheet.stackIcon}
+            onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         
@@ -201,6 +225,21 @@ const MainNavigator = createDrawerNavigator(
                 drawerIcon: ({tintColor}) =>(
                     <Icon
                         name='tree'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+           }
+        },
+
+       Favorites: { 
+            screen: FavoritesNavigator,
+            navigationOptions:{
+                drawerLabel: 'My Favorites',
+                drawerIcon: ({tintColor}) =>(
+                    <Icon
+                        name='heart'
                         type='font-awesome'
                         size={24}
                         color={tintColor}
